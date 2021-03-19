7fef5b9e488ee8a7d4c70bf08f2a52e06df8c7bc
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-03-19 20:58:11 +0900

    완료

---
+++ for문/10950.py
@@ -0,0 +1,14 @@
+import sys
+
+answer = []
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    c = a+b
+    answer.append(c)
+
+for i in range(n):
+    sys.stdout.write(str(answer[i]) + '\n')
+
