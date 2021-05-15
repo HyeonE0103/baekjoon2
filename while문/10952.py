b646c995ec28207027c2518ab29eb26e1aa1f737
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-05-15 23:10:27 +0900

    완료

---
+++ while문/10952.py
@@ -0,0 +1,13 @@
+import sys
+
+number = []
+
+while True:
+    a, b = map(int, sys.stdin.readline().split())
+    if a == 0 and b == 0:
+        break
+    c = a+b
+    number.append(c)
+
+for i in number:
+    sys.stdout.write(str(i) + '\n')
